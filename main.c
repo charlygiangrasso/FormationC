829cd624a01ecce49007d3942f36d348e4cbb927
Nadir GHARBI <n.gharbi@avis2sante.fr>
2025-10-05 17:22:14 +0200

    explication des variables et des types en Python et en C

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    // type <nom de la variable> = <valeur du meme type>
+    int entier = 3456;
+    char caractere = 'D'; // => une lettre
+    char caracteres = "Ddd"; // => une phrase
+    float cacaboudin = 3.0; // => pour le caca boudin
+    
+
+    printf("Hello world");
+}
---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Les bases en python
+'''
+
+a = 1 # nombre
+b = 'dssdlkfsdfhj' # chaine de caractere 
+c = 3.0 # flottant
+d = [a, b, c] # tableau 
+e = {
+    "Abricot": "Fruit qui est bon",
+    "Pomme": "Fruit qui pousse sur les pommiers"
+} # dictionnaire => { cle: valeur }
+
