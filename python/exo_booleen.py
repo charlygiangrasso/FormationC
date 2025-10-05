c1ceb2a4637ffe71bbc7c05f585bb12a0840e68f
Charly Giangrasso <charlygiangrassopro@gmail.com>
2025-10-05 19:51:10 +0200

    exercice booleen

---
+++ python/exo_booleen.py
@@ -0,0 +1,20 @@
+"""
+ex1 : demande a l'utilisateur de rentré 2 nombre et affiche si le premier nombre rentré est plus grand que le deuxième
+ex2 : verification mdp, demandé a l'utilisateur de saisir un mdp et affiche si le mdp est égal charly123 
+ex3 : Écris un programme qui demande l’âge d’un utilisateur et : retourne True s’il a entre 18 et 30 ans inclus.
+"""
+
+# exo1
+# a = input("Entrez un nombre : ")
+# b = input("Entrez un nombre : ")
+# print(a > b)
+
+# exo2
+# mdp = input("saisir un mot de passe : ")
+# mdp_demander = 'charly123'
+# print(mdp == mdp_demander)
+
+# #ex3
+# age = input("quelle est votre âge : ")
+# age_demander = 18<= int(age) <=30 
+# print(age_demander)
