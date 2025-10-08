07d5d2aa1495c94171f3875636654277d7921fbf
Nadir GHARBI <n.gharbi@avis2sante.fr>
2025-10-08 19:42:51 +0200

    some changes

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    // type <nom de la variable> = <valeur du meme type>
-    int entier = 3456;
-    char caractere = 'D'; // => une lettre
-    char caracteres = "Ddd"; // => une phrase
-    float cacaboudin = 3.0; // => pour le caca boudin
-    
-
     printf("Hello world");
+    return 0; 
 }
